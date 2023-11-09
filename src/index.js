1e93ae8cb06b76d6ef5b6ea07ee712e49d32af67
Bianca <bibia.the.rox@gmail.com>
2023-11-09 17:47:54 -0300

    Adicionado cors

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ require("dotenv").config();
 const express = require ("express");
 const rotas = require("./rotas");
 const knex = require("./conexao");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(rotas);
